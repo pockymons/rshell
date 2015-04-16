4eee3748df73da753985ba1ac5156349f2e45219
Joshua Wong <joshwong96dev@gmail.com>
2015-04-16 00:51:20 -0700

    Updated rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
-#include <boost/tokenizer.hpp>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <boost/tokenizer.hpp> //Boost tokenizer
+#include <unistd.h> // Fork()
+#include <sys/types.h> // Wait()
+#include <sys/wait.h> // Wait()
 #include <vector>
+#include <stdio.h> //Perror()
+#include <errno.h> // Perror()
+#include <algorithm>
 
 using namespace std;
 using namespace boost;
-
-#define TOKENIZER tokenizer<char_seperator<char>> 
 
 int main()
 {
@@ -27,15 +28,34 @@ int main()
 			commandLine = commandLine.substr(commandLine.find('#'));
 		}
 
-		TOKENIZER tok(commandLine);
-		for(TOKENIZER::iterator iter = tok.begin(); iter != tok.end(); ++iter)
+		// Finds locations of connectors; a && b, && has a location of 3
+		vector<int> connectorLocs;
+		unsigned int marker = 0; // Marks location to start find() from
+		while((int loc = commandLine.find(" && ", marker)) != -1) 
+		{
+			connectorLocs.push_back(loc + 1); // Add 1 so that the location is &, not space
+			marker = loc + 4; // Starts searching after " && "
+		}
+		marker = 0;
+		while((int loc = commandLine.find(" || ", marker)) != -1) 
+		{
+			connectorLocs.push_back(loc + 1); // Add 1 so that the location is &, not space
+			marker = loc + 4; // Starts searching after " && "
+		}
+		marker = 0;
+		while((int loc = commandLine.find(" ; ", marker)) != -1) 
+		{
+			connectorLocs.push_back(loc + 1); // Add 1 so that the location is &, not space
+			marker = loc + 3; // Starts searching after " && "
+		}
+		sort(connectorLocs.begin(), connectorLocs.end());
+
+		char_seperator<char> delim(" ");
+		tokenizer<char_seperator<char>> tok(commandLine, delim);
+		for(auto iter : tok) // = tok.begin(); iter != tok.end(); ++iter)
 		{			
 			string command;
 			command = *iter;
-			while(iter != tok.end() && *iter != "&&" *iter != "||" && *iter != ";")
-			{
-				++iter;
-			}
 		}
 	}
 	return 0;
