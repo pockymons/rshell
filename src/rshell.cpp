9eea69e5b9cd1f608653dd9d2702c2b69e483f87
Joshua Wong <joshwong96dev@gmail.com>
2015-05-15 00:45:01 -0700

    Output redirection finished?

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -4,11 +4,13 @@
 #include <unistd.h> // Fork()
 #include <sys/types.h> // Wait()
 #include <sys/wait.h> // Wait()
+#include <sys/stat.h>
 #include <vector>
 #include <stdio.h> //Perror()
 #include <errno.h> // Perror()
 #include <algorithm>
 #include <utility>
+#include <fcntl.h>
 
 using namespace std;
 using namespace boost;
@@ -30,6 +32,93 @@ bool cStringEqual(char* c1, char* c2)
 		return false;
 	}
 	return true;
+}
+
+// file is the file to be outputted to
+// replaceFD is the file directory to copy to
+// numBrackets == 1 if ">", numBrackets == 2 if ">>"
+// used in child process; uses _exit() for that reason
+void outputRedir(string file, int replaceFD, int numBrackets)
+{
+	int fdOpen;
+	if(numBrackets == 1)
+	{
+		if(-1 == (fdOpen = open(file.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)))
+		{
+			perror("Open");
+			_exit(1);
+		}
+	}
+	if(numBrackets == 2)
+	{
+		if(-1 == (fdOpen = open(file.c_str(), O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR)))
+		{
+			perror("Open at");
+			_exit(1);
+		}
+	}
+	
+	if(-1 == dup2(fdOpen, replaceFD))
+	{
+		perror("Open");
+		_exit(1);
+	}
+	
+	if(-1 == close(fdOpen))
+	{
+		perror("Close");
+		_exit(1);
+	}
+}
+
+// file is the file to be outputted to
+// replaceFD is the file directory to copy to
+// numBrackets == 1 if "<", numBrackets == 3 if "<<<"
+// Used in child process
+void inputRedir(string file, int replaceFD, int numBrackets)
+{
+	int fdInput;
+	if(numBrackets == 1)
+	{
+		if(-1 == (fdInput = open(file.c_str(), O_RDONLY)))
+		{
+			perror("Open");
+			_exit(1);
+		}
+		if(-1 == dup2(fdInput, replaceFD))
+		{
+			perror("Open");
+			_exit(1);
+		}
+		
+		if(-1 == close(fdInput))
+		{
+			perror("Close");
+			_exit(1);
+		}
+	}
+	if(numBrackets == 3)
+	{
+		// In this case, file isn't actually a file; I just didn't rename it; in this case, file is the string to be redirected
+		char buf[BUFSIZ];
+		for(int i = 0; i < BUFSIZ; ++i)
+		{
+			buf[i] = file.at(i);
+			file.erase(0, BUFSIZ);
+		}
+		while(file.size() > 0)
+		{
+			if(-1 == write(replaceFD, buf, BUFSIZ))
+			{
+				perror("Write");
+			}
+			for(int i = 0; i < BUFSIZ; ++i)
+			{
+				buf[i] = file.at(i);
+				file.erase(0, BUFSIZ);
+			}
+		}
+	}
 }
 
 int main()
@@ -176,7 +265,7 @@ int main()
 				string::size_type iRedir3 = 0;
 				while((iRedir1 = tokenString.find("<")) != string::npos)
 				{
-					iRedir3 = tokenString.find("<<");
+					iRedir3 = tokenString.find("<<<");
 					unsigned int offset = 0;
 					if(iRedir1 < iRedir3)
 					{
@@ -201,11 +290,11 @@ int main()
 					pair<string, string> tempPair("", redirFile);	
 					if(offset == 1)
 					{
-						tempPair.first = ">";
+						tempPair.first = "<";
 					}
 					else
 					{
-						tempPair.first = ">>>";
+						tempPair.first = "<<<";
 					}
 					ioFiles.push_back(tempPair);
 					
@@ -227,7 +316,7 @@ int main()
 			args.push_back(NULL); // NULL terminating at the end of vector/array
 			
 			//Blank command or consecutive connectors
-			if(args.size() == 1)
+			if(args.size() == 1 && ioFiles.size() == 0)
 			{
 				continue;
 			}
@@ -235,7 +324,7 @@ int main()
 			char* exitCString = const_cast<char*> ("exit"); 
 				
 			//cout << cStringEqual(args.at(0), exitCString) << endl; // DEBUGGING
-			if(cStringEqual(args.at(0), exitCString)) // if command is exit, exit shell
+			if(args.size() > 1 && cStringEqual(args.at(0), exitCString)) // if command is exit, exit shell
 			{
 				exit(0);
 			}
@@ -251,6 +340,32 @@ int main()
 			{
 				if(pid == 0) // Child process
 				{
+					// Reminder: vector<pair<string, string>> ioFiles; First string is which io redirect (<, >, >>), second is file
+					for(auto iter = ioFiles.begin(); iter != ioFiles.end(); ++iter)
+					{
+						if(iter->first == ">")
+						{
+							outputRedir(iter->second, 1, 1);
+						}
+						else if(iter->first == ">>")
+						{
+							outputRedir(iter->second, 1, 2);
+						}
+						else if(iter->first == "<")
+						{
+							inputRedir(iter->second, 0, 1);
+						}
+						else if(iter->first == "<<<")
+						{
+							inputRedir(iter->second, 0, 3);
+						}
+						else
+						{
+							// Shouldn't ever happen but just in case
+							cerr << "IO redirection error" << endl;
+							_exit(1);
+						}
+					}
 					execvp(args.at(0), &(args[0]));
 					// Following don't run if execvp succeeds
 					perror("Command execution error");
